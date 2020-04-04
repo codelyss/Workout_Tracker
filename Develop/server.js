71bd0dcd48f8f8c256fb54a80fb7d85f511d3142
codelyss <atcodeveloper@gmail.com>
2020-04-03 22:02:44 -0400

    Included the seed but made sure that it doesn't stop the application

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const db = require('./database');
+const seed = require('./seeders');
 
 const PORT = process.env.PORT || 3000;
 
